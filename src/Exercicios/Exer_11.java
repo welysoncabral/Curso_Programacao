88913b0297b13ba997eeb0e4154f4b0c96a3bdd7
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-11-02 11:34:35 -0300

    Exer_11

--- src/Exercicios/Exer_11.java
+++ src/Exercicios/Exer_11.java
@@ -7,6 +7,7 @@ public class Exer_11 {
     public static void main(String[] args) {
         int cod ;
         int quant;
+        double total = 0;
 
         System.out.println("Digite o código do produto:");
         Locale.setDefault(Locale.US);
@@ -16,6 +17,31 @@ public class Exer_11 {
         System.out.println("Digite a quantidade do produto:");
         Locale.setDefault(Locale.US);
         quant = sc.nextInt();
+
+        if (cod == 1) {
+            total = quant * 4.0;
+            System.out.printf("Total: R$ %.2f%n", total);
+        }
+        else if (cod == 2) {
+            total = quant * 4.5;
+            System.out.printf("Total: R$ %.2f%n", total);
+        }
+        else if (cod == 3) {
+            total = quant * 5.0;
+            System.out.printf("Total: R$ %.2f%n", total);
+        }
+        else if (cod == 4) {
+            total = quant * 2.0;
+            System.out.printf("Total: R$ %.2f%n", total);
+        }
+        else if (cod == 5) {
+            total = quant * 1.5;
+            System.out.printf("Total: R$ %.2f%n", total);
+        }
+        else{
+            System.out.println("Voce digitou um número incorreto tente novamente!");
+        }
+
 
     }
 }
