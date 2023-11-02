f42b2332bdd899272caa93b226a23192db0dc9e3
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-11-02 16:05:52 -0300

    Exer_11

---
+++ src/Exercicios/Condicao_Ternaria.java
@@ -0,0 +1,17 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Condicao_Ternaria {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+
+        sc.close();
+    }
+}
