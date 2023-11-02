d90de63d91f29f338e247585c9831440328c47b5
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-11-02 10:55:48 -0300

    Exer_11

--- src/Exercicios/Exer_11.java
+++ src/Exercicios/Exer_11.java
@@ -1,11 +1,21 @@
 package Exercicios;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exer_11 {
     public static void main(String[] args) {
         int cod ;
         int quant;
 
-        System.out.println("Digite o código do produto");
+        System.out.println("Digite o código do produto:");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        cod = sc.nextInt();
+
+        System.out.println("Digite a quantidade do produto:");
+        Locale.setDefault(Locale.US);
+        quant = sc.nextInt();
 
     }
 }
