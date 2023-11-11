481f560a8a37e6fdb4350482e651137aa6e93003
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-11-11 13:21:07 -0300

    While teste de mesa

---
+++ src/Exercicios/Exer_13.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class Exer_13 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y= 0;
+
+        while (x > 2) {
+            System.out.print(x);
+            y = y + x;
+            x = x - 1;
+        }
+    }
+}
