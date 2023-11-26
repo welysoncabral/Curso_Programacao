6a6cd415ddc79f85b3e54fb7c9e382d8fdfe26db
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-11-26 02:10:22 -0300

    Update ParaRep3.java

--- src/ParaRep3.java
+++ src/ParaRep3.java
@@ -1,6 +1,5 @@
 public class ParaRep3 {
         public static void main(String[] args) {
-
             for (int i=4; i>=0; i--){
                 System.out.println("Valor de i:" + i);
             }
