a3e03e3bf825c9f64ebc63ed0fffb33f0426eab4
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-11-26 02:09:23 -0300

    Comando de repetição for

---
+++ src/ParaRep3.java
@@ -0,0 +1,8 @@
+public class ParaRep3 {
+        public static void main(String[] args) {
+
+            for (int i=4; i>=0; i--){
+                System.out.println("Valor de i:" + i);
+            }
+        }
+    }
