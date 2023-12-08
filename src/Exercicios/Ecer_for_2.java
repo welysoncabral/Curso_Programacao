4739b7afe4cac620478855c51782c54547d2e6cb
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-12-08 00:53:23 -0300

    Exercício For

---
+++ src/Exercicios/Ecer_for_2.java
@@ -0,0 +1,31 @@
+package Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ecer_for_2 {
+    public static void main(String[] args) {
+
+        int in = 0;
+        int out = 0;
+        int N;
+
+        System.out.println("Digite um número inteiro:");
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        for (int i=0; i<N; i++) {
+            int x = sc.nextInt();
+            if (x >= 10 && x <= 20) {
+                in = in + 1;
+            }
+            else {
+                out = out + 1;
+            }
+        }
+
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+        sc.close();
+    }
+}
