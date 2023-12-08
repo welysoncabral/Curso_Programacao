95bf8aa04fac049b8a26a472add2ec0dfb3f468b
welysoncabral <68700857+welysoncabral@users.noreply.github.com>
2023-12-08 01:16:07 -0300

    Exercício For

---
+++ src/Exercicios/Exer_for_4.java
@@ -0,0 +1,27 @@
+package Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer_for_4 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (y == 0) {
+            System.out.println("divisao impossivel");
+        }
+        else {
+            double div = (double) x / y;
+            System.out.printf("%.1f%n", div);
+        }
+    }
+        sc.close();
+    }
+
+ }
